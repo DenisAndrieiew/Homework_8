1561e5aec56a35633572db8c03b6233aa61e327b
DenisAndrieiew <eretarmineret@gmail.com>
2021-08-26 22:22:52 +0300

    add data to dml.sql add Readme.md

--- src/main/resources/dml.sql
+++ src/main/resources/dml.sql
@@ -1,3 +1,32 @@
 INSERT INTO roles (identifier, role)
-VALUES ('f70f4921-cda4-4bf5-9152-0b1adee1e4a7', 'ROLE_USER'),
-('d0b608ae-3277-4bb4-904a-c40644c8bb85', 'ROLE_ADMIN');
+VALUES (gen_random_uuid(),, 'ROLE_USER'),
+(gen_random_uuid(),, 'ROLE_ADMIN');
+
+
+INSERT INTO manufacturers
+(identifier, manufacturer_name)
+VALUES
+(gen_random_uuid(), 'carpentry cut down with an ax'),
+(gen_random_uuid(), 'Iron forge metalwork'),
+(gen_random_uuid(), 'Luxury Jewelery');
+
+
+INSERT INTO products
+(identifier, product_name, product_cost, manufacturer_id)
+VALUES
+(gen_random_uuid(), 'Rusty nail', 10,
+(SELECT identifier FROM manufacturers WHERE manufacturer_name = 'Iron forge metalwork')),
+(gen_random_uuid(), 'Iron rail', 600,
+(SELECT identifier FROM manufacturers WHERE manufacturer_name = 'Iron forge metalwork')),
+
+(gen_random_uuid(), 'Uncomfortable chair', '50',
+(SELECT identifier FROM manufacturers WHERE manufacturer_name = 'carpentry cut down with an ax')),
+(gen_random_uuid(), 'wooden frame', 30,
+(SELECT identifier FROM manufacturers WHERE manufacturer_name = 'carpentry cut down with an ax')),
+(gen_random_uuid(), 'splintered table', 100,
+(SELECT identifier FROM manufacturers WHERE manufacturer_name = 'carpentry cut down with an ax')),
+
+(gen_random_uuid(), 'Golden ring', 1000,
+(SELECT identifier FROM manufacturers WHERE manufacturer_name = 'Luxury Jewelery')),
+(gen_random_uuid(), 'precious necklace', 2000,
+(SELECT identifier FROM manufacturers WHERE manufacturer_name = 'Luxury Jewelery')),
